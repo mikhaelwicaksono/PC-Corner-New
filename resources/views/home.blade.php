8a7ddba9eb3c11f2c09e019c155fae02dadb141c
Muhammad Fiqri Febriansyah <105426015+fiqrifebrians@users.noreply.github.com>
2023-01-19 17:07:13 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,38 @@
-@extends('template.master')
-@section('content')
-    <div class="container"></div>
-    @include('template.header')
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <div class="card text-center">
+        <img src="{{url('/assets/pc.jpg')}}" class="img-fluid opacity-25" alt="Background Image">
+        <div class="container text-center">
+            <div class="card-img-overlay">
+                <ul class="nav nav-pills d-flex justify-content-center">
+                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                  </ul>
+                <div class="row d-flex h-100">
+                    <div class="col align-self-center">
+                        <img src="{{url('/assets/logo.png')}}" alt="">
+                        <p class="card-text text-dark w-50 p-3">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <a href="/" class="btn btn-primary fs-5">Register</a>
+                        <div id="button" class="form-text text-center mb-4">Already Registered? <a
+                            href="{{ route('login') }}">Sign in here</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>

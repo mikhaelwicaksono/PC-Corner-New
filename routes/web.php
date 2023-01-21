02f6e739367d838e6523f5610c1fe83b0dc5d64b
fiqrifebrians <febriansfiqri@gmail.com>
2023-01-21 22:22:42 +0700

    ganti welcom jadi home

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::get('/register', [UserController::class, 'register'])->name('register')->middleware('guest');
 Route::post('/register', [UserController::class, 'register_auth'])->name('register')->middleware('guest');

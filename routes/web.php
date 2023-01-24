1dba6df17c6d6753c5819c5512920e09d6ac93ae
mikhaelwicaksono <nirwani96@gmail.com>
2023-01-24 12:51:25 +0700

    Update di route, dimasukin ke middleware

--- routes/web.php
+++ routes/web.php
@@ -35,14 +35,14 @@ Route::middleware(['auth'])->group(function () {
 
         return redirect('login');
     })->name('logout');
-});
-Route::get('/request/{b}', [RequestsController::class, 'viewing']);
-Route::post('/load/{c}', [RequestsController::class, 'adding']);
-Route::get('/updatepassword/{s}', [UserController::class, 'viewupdate']);
-Route::post('/executedpassword/{l}', [UserController::class, 'updatingpassword']);
-Route::get('/profile/{b}',[UserController::class,'display']);
+    Route::get('/request/{b}', [RequestsController::class, 'viewing']);
+    Route::post('/load/{c}', [RequestsController::class, 'adding']);
+    Route::get('/updatepassword/{s}', [UserController::class, 'viewupdate']);
+    Route::post('/executedpassword/{l}', [UserController::class, 'updatingpassword']);
+    Route::get('/profile/{b}', [UserController::class, 'display']);
 
-Route::get('/home', [HomeController::class, 'showName']);
-Route::get('/about', [HomeController::class, 'about']);
-Route::get('/contact', [HomeController::class, 'contact']);
-Route::get('/history', [HistoryController::class, 'view']);
+    Route::get('/home', [HomeController::class, 'showName']);
+    Route::get('/about', [HomeController::class, 'about']);
+    Route::get('/contact', [HomeController::class, 'contact']);
+    Route::get('/history', [HistoryController::class, 'view']);
+});
